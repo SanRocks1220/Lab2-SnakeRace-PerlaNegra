751f9cf5a0afb7fecb627b1b9a86b559c7ddbcdf
Santiago Andrés Rocha <99696682+SanRocks1220@users.noreply.github.com>
2023-08-29 20:06:23 -0500

    Update Main.java

--- wait-notify-excercise-master/src/main/java/edu/eci/arsw/primefinder/Main.java
+++ wait-notify-excercise-master/src/main/java/edu/eci/arsw/primefinder/Main.java
@@ -1,5 +1,7 @@
 package edu.eci.arsw.primefinder;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -7,6 +9,28 @@ public class Main {
         
         control.start();
 
+        int time = Control.getTmiliseconds();
+
+        Scanner line = new Scanner(System.in);
+        String enter = "";
+
+        while(enter == ""){
+            try {
+                Control.sleep(time);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            
+            control.stopThreads();
+            control.printNumPrimes();
+
+            enter = line.nextLine();
+            if(enter == ""){
+                control.resumeThreads();
+            }
+        }
+
+        line.close();
     }
 	
 }
