017f88996fed31eaa2e7337ad3a269c947a9062c
Santiago Andrés Rocha <99696682+SanRocks1220@users.noreply.github.com>
2023-08-29 23:03:09 -0500

    Parte 1 sobre hilos y zona crítica finalizada.

--- wait-notify-excercise-master/src/main/java/edu/eci/arsw/primefinder/Control.java
+++ wait-notify-excercise-master/src/main/java/edu/eci/arsw/primefinder/Control.java
@@ -1,44 +1,77 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package edu.eci.arsw.primefinder;
 
-/**
- *
- */
+import java.util.Scanner;
+
 public class Control extends Thread {
-    
-    private final static int NTHREADS = 3;
-    private final static int MAXVALUE = 30000000;
-    private final static int TMILISECONDS = 5000;
 
-    private final int NDATA = MAXVALUE / NTHREADS;
+	private final static int NTHREADS = 3;
+	private final static int MAXVALUE = 30000000;
+	private final static int TMILISECONDS = 5000;
 
-    private PrimeFinderThread pft[];
-    
-    private Control() {
-        super();
-        this.pft = new  PrimeFinderThread[NTHREADS];
+	private final int NDATA = MAXVALUE / NTHREADS;
 
-        int i;
-        for(i = 0;i < NTHREADS - 1; i++) {
-            PrimeFinderThread elem = new PrimeFinderThread(i*NDATA, (i+1)*NDATA);
-            pft[i] = elem;
-        }
-        pft[i] = new PrimeFinderThread(i*NDATA, MAXVALUE + 1);
-    }
-    
-    public static Control newControl() {
-        return new Control();
-    }
+	private PrimeFinderThread pft[];
 
-    @Override
-    public void run() {
-        for(int i = 0;i < NTHREADS;i++ ) {
-            pft[i].start();
-        }
-    }
-    
+	private Control() {
+		super();
+		this.pft = new PrimeFinderThread[NTHREADS];
+
+		int i;
+		for (i = 0; i < NTHREADS - 1; i++) {
+			PrimeFinderThread elem = new PrimeFinderThread(i * NDATA, (i + 1) * NDATA);
+			pft[i] = elem;
+		}
+		pft[i] = new PrimeFinderThread(i * NDATA, MAXVALUE + 1);
+	}
+
+	public static Control newControl() {
+		return new Control();
+	}
+
+	public synchronized void pauseThreads() {
+		for (PrimeFinderThread thread : pft) {
+			thread.pauseThread();
+		}
+	}
+
+	public synchronized void resumeThreads() {
+		for (PrimeFinderThread thread : pft) {
+			thread.resumeThread();
+		}
+	}
+
+	@Override
+	public void run() {
+		for (int i = 0; i < NTHREADS; i++) {
+			pft[i].start();
+		}
+
+		Scanner scanner = new Scanner(System.in);
+        int size = 0;
+        int size2 = 0;
+		while (true) {
+			try {
+				Thread.sleep(TMILISECONDS);
+				pauseThreads();
+
+                size = pft[0].getPrimes().size();
+                if(size != size2){
+                    System.out.println("Cantidad de Primos encontrados por los Hilos: " + size);
+                    size2 = size;
+                    System.out.println("Presiona ENTER para continuar...");
+                    scanner.nextLine();
+                    resumeThreads();
+                } else {
+                    System.out.println("No hay más primos que encontrar.");
+                    System.out.println("Total Primos: " + size);
+                    break;
+                }
+				
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
+
 }
